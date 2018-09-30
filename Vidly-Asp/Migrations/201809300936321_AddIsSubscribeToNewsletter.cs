b7d80faa941e236cb15e98d94fa4500b5ffbbb13
Harsh Kumar (IN6076) <m1036076@mindtree.com>
2018-09-30 15:20:44 +0530

    Add project files.

---
+++ Vidly-Asp/Migrations/201809300936321_AddIsSubscribeToNewsletter.cs
@@ -0,0 +1,18 @@
+namespace Vidly_Asp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsSubscribeToNewsletter : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "IsSubscribedToNewsletter", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "IsSubscribedToNewsletter");
+        }
+    }
+}
---
+++ Vidly-Asp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_Asp.Startup))]
+namespace Vidly_Asp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
